7f26fd07b2f4f8d689cab5135f29e4be5fe5b12c
tomcz <github@tomczarniecki.com>
2010-06-26 23:24:24 +1000

    HTML 4.01 compliant

--- src/main/example/framework/servlet/ApplicationServlet.java
+++ src/main/example/framework/servlet/ApplicationServlet.java
@@ -11,9 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
+import static org.apache.commons.lang.StringUtils.defaultString;
+
 public class ApplicationServlet extends HttpServlet {
 
     private Application application;
+    private String version;
 
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
@@ -21,14 +25,17 @@ public class ApplicationServlet extends HttpServlet {
 
         ServletContext context = servletConfig.getServletContext();
         application = (Application) context.getAttribute(Application.class.getName());
+
+        String showVersion = servletConfig.getInitParameter("version-tag");
+        version = "per-request".equals(showVersion) ? null : showVersion;
     }
 
     @Override
     protected void service(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
             throws ServletException, IOException {
 
+        servletRequest.setAttribute("version", defaultString(version, randomAlphanumeric(7)));
         Response response = application.process(new ServletRequestContext(servletRequest));
         response.render(new ServletResponseContext(servletRequest, servletResponse));
     }
-
 }
