bc6e2c02085b2be761746f75c3ce09104d8da7ff
tomcz <github@tomczarniecki.com>
2011-09-18 02:52:45 +1000

    remove unused code

--- src/main/example/framework/template/TemplateComponent.java
+++ src/main/example/framework/template/TemplateComponent.java
@@ -7,6 +7,5 @@ public class TemplateComponent extends ComponentAdaptor {
 
     public void registerApplicationScope(Container scope) {
         scope.register(FreemarkerTemplateFactory.class);
-//        scope.register(WebTemplateFactory.class, scope.newArgs().autowired().constant("/templates"));
     }
 }
